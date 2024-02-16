8bdacaedf182ad940516add41015711af2e0909c
Anatolii Petrov <hello@aswitch.me>
2024-02-16 20:57:06 +0100

    Initial Commit

---
+++ LL(dinnerMenu)/LL_dinnerMenu_App.swift
@@ -0,0 +1,17 @@
+//
+//  LL_dinnerMenu_App.swift
+//  LL(dinnerMenu)
+//
+//  Created by Anatoliy Petrov on 16.2.24..
+//
+
+import SwiftUI
+
+@main
+struct LL_dinnerMenu_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
